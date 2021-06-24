1f643fdd7d77f4c14e45a2ea780f769f8e622428
92968 <86350889+92968@users.noreply.github.com>
2021-06-24 23:25:34 +0700

    Update k_127589_r.sh

--- k_127589_r.sh
+++ k_127589_r.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x51f707e941a5a31299c05292ff2bc40e10783147 -p x -w F
+./t-rex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0xb35092a7c6f60cd2ced8fe7062eb571d31f3e017 -p x -w K
